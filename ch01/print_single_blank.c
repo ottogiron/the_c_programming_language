29c567ca024a04cb22c518649879d824000a04ad
Otto Giron <ottog2486@gmail.com>
2018-09-06 18:43:52 -0600

    exersice replace to single blank

---
+++ ch01/print_single_blank.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{   
+    int c, b;
+
+    while( (c = getchar()) != EOF ) {
+        if (c == ' ')
+         while ( (b = getchar()) == ' ')
+            ;
+        putchar(c);
+    }
+    return 0;
+}
