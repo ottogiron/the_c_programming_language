85fddd55e4052b253dd64ae489f004e864f7298b
Otto Giron <ottog2486@gmail.com>
2018-10-08 18:39:08 -0600

    implement detab exercise

--- ch01/detab-ex1_20.c
+++ ch01/detab-ex1_20.c
@@ -3,34 +3,20 @@
 #define N  4
 #define MAX_LEN 3000
 
-int gettline(char line[], int max);
-void detab(char line[]);
 
 int main()
 {
-	char  line[MAX_LEN];	
+	int c, j;
+
+	while ((c = getchar()) != EOF)
+	{
+	   	if(c == '\t')
+			for(j = 0; j< N; ++j)
+			putchar(' ');
+		else 
+			putchar(c);
+	}
 
 	return 0;
 }
-
-//getline requires a MAX variable because the STDIN doesn provide a null character to stop :'(
-int gettline(char line[], int max)
-{
-	int i, c;
-
-	for (i = 0; i < max -1 && (c = getchar()) != EOF && c != '\n'; ++i ){
-		line[i] = c;
-
-	}
-
-	if (c == '\n'){
-
-		line[i] = c;
-		++i;
-	}
-	line[i] = '\0';
-	return i;
-}
-
-
 
