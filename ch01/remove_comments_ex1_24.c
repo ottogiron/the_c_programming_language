751ca8adb392b925440bd2a2f93fcc8c5279c3bb
Otto Giron <ottog2486@gmail.com>
2018-10-16 00:42:02 -0600

    functional remove comments program

--- ch01/remove_comments_ex1_24.c
+++ ch01/remove_comments_ex1_24.c
@@ -5,33 +5,32 @@ int gettline(char line[], int max);
 
 int main(void) {
 	int len, c, i, in_comment;
-	int first_slash, second_slash;
-	int first_asterisk, second_asterisk, second_slash_asterisk;
+	int first_slash;
+	int second_asterisk;
 	int in_line_comment, in_asterisk_comment;
 	char line[MAX_LEN];
 	in_comment = 0;
 	first_slash = 0;
-	first_asterisk = 0, second_asterisk = 0;
+	second_asterisk = 0;
 	in_line_comment = 0, in_asterisk_comment = 0;
 	while ((len = gettline(line, MAX_LEN)) > 0) {
 		for (i = 0; i < len; ++i) {
 			c = line[i];
 			if (c == '/') {
-				if (!first_slash)
-					first_slash = 1;
-				else if (first_slash)
-					in_line_comment = 1;
-				else if (second_asterisk) {
+				if (second_asterisk) {
 					first_slash = 0;
 					in_asterisk_comment = 0;
 					second_asterisk = 0;
-				}
-
+					putchar('\n');
+				} else if (!first_slash)
+					first_slash = 1;
+				else if (first_slash)
+					in_line_comment = 1;
 			} else if (c == '*') {
-				if (first_slash)
-					in_asterisk_comment = 1;
-				else if (in_asterisk_comment)
+				if (in_asterisk_comment)
 					second_asterisk = 1;
+				else if (first_slash)
+					in_asterisk_comment = 1;
 			} else if (c == '\n') {
 				if (in_line_comment) {
 					first_slash = 0;
@@ -40,8 +39,8 @@ int main(void) {
 				if (!in_asterisk_comment) putchar('\n');
 			} else {
 				if (first_slash) first_slash = 0;
-				if (!in_line_comment &&
-				    !in_asterisk_comment) {  // not in comment
+				if (!(in_line_comment ||
+				      in_asterisk_comment)) {  // not in comment
 					putchar(c);
 				}
 			}
